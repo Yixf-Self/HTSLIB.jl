79c6b773ecdbcf459382c9e6183691a59c2644d2
guolanting <guolanting@haplox.com>
2016-01-21 10:24:11 +0000

    depprecated previous codes

--- test/sam.jl
+++ test/sam.jl
@@ -4,7 +4,9 @@ const SAM_FL = "data/test.sam"
 fp = HTSLIB.sam_open(SAM_FL,"r")
 @test typeof(fp) == Ptr{Void}
 
-@show HTSLIB.sam_hdr_read(fp)
+header = HTSLIB.sam_hdr_read(fp)
+aln = HTSLIB.bam_init1()
+HTSLIB.sam_read1()
 
-#@test HTSLIB.sam_close(fp) == 0
+@test HTSLIB.sam_close(fp) == 0
 
