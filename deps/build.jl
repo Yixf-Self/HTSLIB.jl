4563e476d5653f4de1dd56af50b3cb0866ac0364
guolanting <guolanting@haplox.com>
2016-02-15 12:09:40 +0800

    fix a typo of cd

--- deps/build.jl
+++ deps/build.jl
@@ -2,7 +2,7 @@ libhts =  "libhts.so"
 if !in(libhts, readdir("/usr/local/lib"))
     cmd = `git clone https://github.com/samtools/htslib.git`
     dir = dirname(@__FILE__)
-    cd("dir") do
+    cd(dir) do
         success(cmd) || error("git clone htslib failed")
         success(`autoconf`) || error("htslib autoconf failed")
         success(`./configure`) || error("htslib configure failed")
