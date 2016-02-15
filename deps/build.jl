bcaee73f84919c1a16c98d56653c71b58be00b27
guolanting <guolanting@haplox.com>
2016-02-15 11:32:19 +0800

    change if end to short circirt

--- deps/build.jl
+++ deps/build.jl
@@ -1,10 +1,10 @@
 libhts =  "libhts.so"
-if !in(libhts,readdir("/usr/local/lib"))
+if !in(libhts, readdir("/usr/local/lib"))
     cmd = `git clone https://github.com/samtools/htslib.git`
-    if !success(cmd) error("git clone htslib failed") end
-    if !success(`autoconf`) error("htslib autoconf failed") end
-    if !success(`./configure`) error("htslib configure failed") end
-    if !success(`make`) error("htslib make failed") end
-    if !success(`sudo make install`) error("sudo make install") end
-    if !success(`sudo cp libhts.so /usr/local/lib`) error("sudo cp libhts.so /usr/local/lib failed") end
+    success(cmd) || error("git clone htslib failed")
+    success(`autoconf`) || error("htslib autoconf failed")
+    success(`./configure`) || error("htslib configure failed")
+    success(`make`) || error("htslib make failed")
+    success(`sudo make install`) || error("sudo make install")
+    success(`sudo cp libhts.so /usr/local/lib`) || error("sudo cp libhts.so /usr/local/lib failed")
 end
