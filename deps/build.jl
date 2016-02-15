3b3f98cc87c336db40089cc099ec24ddb3781430
guolanting <guolanting@haplox.com>
2016-02-15 11:24:40 +0800

    separate each cmds in build

--- deps/build.jl
+++ deps/build.jl
@@ -2,9 +2,9 @@ libhts =  "libhts.so"
 if !in(libhts,readdir("/usr/local/lib"))
     cmd = `git clone https://github.com/samtools/htslib.git`
     if !success(cmd) error("git clone htslib failed") end
-    if !(success(`autoconf`) && success(`./configure`) && success(`make`))
-        error("install htslib failed")
-    end
+    if !success(`autoconf`) error("htslib autoconf failed") end
+    if !success(`./configure`) error("htslib configure failed") end
+    if !success(`make`) error("htslib make failed") end
     if !success(`sudo make install`) error("sudo make install") end
     if !success(`sudo cp libhts.so /usr/local/lib`) error("sudo cp libhts.so /usr/local/lib failed") end
 end
