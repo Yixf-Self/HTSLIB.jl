75c4066a5ca7e9b83d975014ce9d760ddd229a0d
guolanting <guolanting@haplox.com>
2016-02-15 10:54:06 +0800

    install htslib automacics

--- deps/build.jl
+++ deps/build.jl
@@ -1,1 +1,9 @@
-using BinDeps
+libhts =  "libhts.so"
+if !in(libhts,readdir("/usr/local/lib"))
+    cmd = `git clone https://github.com/samtools/htslib.git`
+    if !sucess(cmd) error("git clone htslib failed") end
+    if !(sucess(`autoconf`) && sucess(`./configure`) && sucess(`make`) && sucess(`sudo make install`))
+        error("install htslib failed")
+    end
+    if !sucess(`sudo cp libhts.so /usr/local/lib`) error("sudo cp libhts.so /usr/local/lib failed") end
+end
