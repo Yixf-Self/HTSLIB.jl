811c70bbfc4165e7991c0b6b94bd40736f73709a
guolanting <guolanting@haplox.com>
2016-02-15 11:37:45 +0800

    cd deps dir and then start build process

--- deps/build.jl
+++ deps/build.jl
@@ -1,10 +1,13 @@
 libhts =  "libhts.so"
 if !in(libhts, readdir("/usr/local/lib"))
     cmd = `git clone https://github.com/samtools/htslib.git`
-    success(cmd) || error("git clone htslib failed")
-    success(`autoconf`) || error("htslib autoconf failed")
-    success(`./configure`) || error("htslib configure failed")
-    success(`make`) || error("htslib make failed")
-    success(`sudo make install`) || error("sudo make install")
-    success(`sudo cp libhts.so /usr/local/lib`) || error("sudo cp libhts.so /usr/local/lib failed")
+    dir = dirname(@__FILE__)
+    cd("dir") do
+        success(cmd) || error("git clone htslib failed")
+        success(`autoconf`) || error("htslib autoconf failed")
+        success(`./configure`) || error("htslib configure failed")
+        success(`make`) || error("htslib make failed")
+        success(`sudo make install`) || error("sudo make install")
+        success(`sudo cp libhts.so /usr/local/lib`) || error("sudo cp libhts.so /usr/local/lib failed")
+    end
 end
