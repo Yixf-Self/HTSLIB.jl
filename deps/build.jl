6d0c16bee1d662b3899d893a8346d5d1bb336ca9
guolanting <guolanting@haplox.com>
2016-02-15 11:08:56 +0800

    separate sudo cmds from other cmds to make diagnostic easy

--- deps/build.jl
+++ deps/build.jl
@@ -2,8 +2,9 @@ libhts =  "libhts.so"
 if !in(libhts,readdir("/usr/local/lib"))
     cmd = `git clone https://github.com/samtools/htslib.git`
     if !success(cmd) error("git clone htslib failed") end
-    if !(success(`autoconf`) && success(`./configure`) && success(`make`) && success(`sudo make install`))
+    if !(success(`autoconf`) && success(`./configure`) && success(`make`))
         error("install htslib failed")
     end
+    if !success(`sudo make install`) error("sudo make install") end
     if !success(`sudo cp libhts.so /usr/local/lib`) error("sudo cp libhts.so /usr/local/lib failed") end
 end
