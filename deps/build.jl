956f547d31e902b4679ef3c5a5919dffdf4b927e
guolanting <guolanting@haplox.com>
2016-02-15 11:04:00 +0800

    fix a typo of success

--- deps/build.jl
+++ deps/build.jl
@@ -1,9 +1,9 @@
 libhts =  "libhts.so"
 if !in(libhts,readdir("/usr/local/lib"))
     cmd = `git clone https://github.com/samtools/htslib.git`
-    if !sucess(cmd) error("git clone htslib failed") end
-    if !(sucess(`autoconf`) && sucess(`./configure`) && sucess(`make`) && sucess(`sudo make install`))
+    if !success(cmd) error("git clone htslib failed") end
+    if !(success(`autoconf`) && success(`./configure`) && success(`make`) && success(`sudo make install`))
         error("install htslib failed")
     end
-    if !sucess(`sudo cp libhts.so /usr/local/lib`) error("sudo cp libhts.so /usr/local/lib failed") end
+    if !success(`sudo cp libhts.so /usr/local/lib`) error("sudo cp libhts.so /usr/local/lib failed") end
 end
