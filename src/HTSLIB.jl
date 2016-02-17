8fef3e62f8cd536ac0e368d90e6d3d14e0146ff1
zhmz90 <zhmz90@gmail.com>
2016-02-17 14:10:36 +0800

    replace Pkg.dir with dirname(dirname(@__FILE__))

--- src/HTSLIB.jl
+++ src/HTSLIB.jl
@@ -1,13 +1,11 @@
 module HTSLIB
 
-const libhts = Libdl.find_library(["libhts.so"],[joinpath(Pkg.dir("HTSLIB"),"deps/usr/lib")])
+const libhts = Libdl.find_library(["libhts.so"],[joinpath(dirname(dirname(@__FILE__)),"deps/usr/lib")])
 
-
-
-include("utils/utils.jl")
 include("bam/bam.jl")
 include("user/bam.jl")
 include("user/sam.jl")
 include("user/query.jl")
+include("utils/utils.jl")
 
 end
