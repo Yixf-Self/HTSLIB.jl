7d04ea4cf871afb431838f060b8165dd6e8b1c46
zhmz90 <zhmz90@gmail.com>
2016-03-07 08:10:21 +0800

    add a test for readline

--- test/user/bam.jl
+++ test/user/bam.jl
@@ -1,6 +1,10 @@
 
 info("Test read bam file")
 data = HTSLIB.readlines("data/100.bam")
+bios = HTSLIB.open("data/100.bam","rb","bam")
+while !HTSLIB.eof(bios)
+    line = HTSLIB.readline(bios)
+end
 #@show data
 info("Read bam successfully")
 
